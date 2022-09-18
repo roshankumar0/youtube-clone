66d35293d90fb71b77925d35a8e3fdeb0f89cddd
roshan kumar <roshankumar40786@gmail.com>
2022-09-18 18:08:30 +0530

    added adds optiom

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-// function myFunction() {
-//     var x = document.getElementsByClassName("yt-aside");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "b";
-//     }
-//   }
 let bars = document.getElementById("bars");
 let aside = document.getElementById("aside")
 bars.addEventListener('click', function () {
     if (aside.style.display === "none") {
         aside.style.display = "block";
+        // aside.style.transform = "translateX(100%)"
+
     }
     else {
         aside.style.display = "none"
